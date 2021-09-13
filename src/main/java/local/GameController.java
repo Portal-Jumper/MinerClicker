bdf6d6bf27ed69e8d27b6e14a65d1d051f180e19
Portal-Jumper <HubertSzymusiuk@gmail.com>
2021-09-13 13:50:45 +0200

    Some more changes

--- src/main/java/local/GameController.java
+++ src/main/java/local/GameController.java
@@ -1,5 +1,18 @@
 package local;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
 public class GameController {
+
+    @FXML
+    ImageView gemImage;
+
+    @FXML
+    void pressGem(){
+        gemImage.setImage(new Image("Ruby.png"));
+    }
 
 }
